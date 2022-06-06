d49f49a180013d4a8f893f567830a8d7f35d9bb1
Anne-15 <wariiyuanne@gmail.com>
2022-06-06 02:20:19 -0700

    printing python lists

---
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -0,0 +1,16 @@
+#include <Python.h>
+#include <object.h>
+#include <listobject.h>
+
+void print_python_list_info(PyObject *p)
+{
+	long int size = PyList_Size(p);
+	int i;
+	PyListObject *obj = (PyListObject *)p;
+
+	printf("[*] Size of the python list = %li\n", size);
+	printf("[*] allocated = %li\n", obj->allocated);
+
+	for (i = 0; i< size; i++)
+		printf("Element %i: %s\n", Py_TYPE(obj->ob_item[i])->tp_name);
+}
